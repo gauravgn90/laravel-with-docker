ab4e76a58fa0ed1c6994b2d3be234dc21297eda7
gauravkumar07nagarro <gaurav.kumar07@nagarro.com>
2022-07-11 20:54:00 +0530

    docker code updated

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,2 +1,3 @@
 CREATE DATABASE IF NOT EXISTS `laravel_with_docker`;
 GRANT ALL ON *.* TO 'root';
+FLUSH PRIVILEGES;
--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 docker-compose down -v && docker-compose build --no-cache && docker-compose up -d
 sleep 15
-docker exec app php artisan migrate
-docker exec app bash -c "chmod -R 777 storage"
-docker exec app php artisan l5-swagger:generate
-docker exec app php vendor/bin/phpunit tests/Unit/
-docker exec app php vendor/bin/phpunit tests/Feature/
-docker exec app bash -c "chmod -R 777 storage/"
+docker exec app php api/artisan
+docker exec app php api/artisan key:generate
+docker exec app php api/artisan migrate
+docker exec app bash -c "chmod -R 777 api/storage"
+docker exec app php api/vendor/bin/phpunit tests/Unit/
+docker exec app php api/vendor/bin/phpunit tests/Feature/
+docker exec app bash -c "chmod -R 777 api/storage/"
