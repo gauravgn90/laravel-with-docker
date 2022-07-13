87d755b1745592343868a7e4b449447eaba4cdce
gauravkumar07nagarro <gaurav.kumar07@nagarro.com>
2022-07-13 23:34:12 +0530

    asd

--- api/routes/web.php
+++ api/routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/ok', function () {
+    return "ok";
+});
+Route::get('/fail', function () {
+    return "failed";
+});
+Route::get('/not-found', function () {
+    return "no found";
+});
---
+++ api/tests/Unit/ExampleTest2.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest2 extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+        //
+    }
+}
